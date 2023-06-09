732642cf4e7a273550ba4924ff0bd2846ff8032b
Emir <elitedesire17@gmail.com>
2023-06-08 22:04:03 -0400

    Finished with the ImageGallery component js & css

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,26 +1,74 @@
 import React, { useState } from "react";
 import styles from "./ImageGallery.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle, faChevronCircleLeft, faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
 
 function ImageGallery({ galleryImages }) {
   const [slideNumber, setSlideNumber] = useState(0);
   const [openModal, setOpenModal] = useState(false);
 
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const nextSlide = () => {
+    slideNumber + 1 === galleryImages.length
+      ? setSlideNumber(0)
+      : setSlideNumber(slideNumber + 1);
+  };
+
+  const prevSlide = () => {
+    slideNumber === 0
+      ? setSlideNumber(galleryImages.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
+
   return (
-    
-    <div className={styles.galleryContainer}>
-      <h1>Gallery</h1>
-      <div className={styles.galleryWrap}>
-        {galleryImages &&
-          galleryImages.map((slide, index) => {
-            return (
-              <div className={styles.single} key={index}>
-                <img src={slide.src} alt="" />
-              </div>
-            );
-          })}
+    <>
+      {openModal && (
+        <div className={styles.slideWrap}>
+          <FontAwesomeIcon
+            icon={faTimesCircle}
+            className={styles.buttonClose}
+            onClick={handleCloseModal}
+          />
+          <FontAwesomeIcon
+            icon={faChevronCircleLeft}
+            className={styles.buttonPrev}
+            onClick={prevSlide}
+          />
+          <FontAwesomeIcon
+            icon={faChevronCircleRight}
+            className={styles.buttonNext}
+            onClick={nextSlide}
+          />
+          <div className={styles.fullScreenImage}>
+            <img src={galleryImages[slideNumber].src} alt={galleryImages[slideNumber].alt} />
+          </div>
+        </div>
+      )}
+      <div className={styles.galleryContainer}>
+        <h1>Gallery</h1>
+        <div className={styles.galleryWrap}>
+          {galleryImages &&
+            galleryImages.map((slide, index) => {
+              return (
+                <div className={styles.single} key={index} onClick={() => handleOpenModal(index)}>
+                  <img src={slide.src} alt={slide.alt} />
+                </div>
+              );
+            })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
 export default ImageGallery;
+
+
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,35 +8,36 @@ import ImageGallery from "../ImageGallery";
 const galleryImages = [
   {
     src: process.env.PUBLIC_URL + "/images/apartments1.png",
-    alt: "Azra Apartments"
+    alt: "Azra Apartments",
   },
   {
-    src: process.env.PUBLIC_URL + "/images/apartments2.JPG",
-    alt: "Azra Apartments"
+    src: process.env.PUBLIC_URL + "/images/apartments2.jpg",
+    alt: "Azra Apartment",
   },
   {
     src: process.env.PUBLIC_URL + "/images/apartmani3.JPG",
-    alt: "Azra Apartments"
+    alt: "Azra Apartments",
   },
   {
     src: process.env.PUBLIC_URL + "/images/apartmani4.JPG",
-    alt: "Azra Apartments"
+    alt: "Azra Apartments",
   },
   {
     src: process.env.PUBLIC_URL + "/images/apartmani5.JPG",
-    alt: "Azra Apartments"
+    alt: "Azra Apartments",
   },
   {
     src: process.env.PUBLIC_URL + "/images/apartmani6.JPG",
-    alt: "Azra Apartments"
-  }
+    alt: "Azra Apartments",
+  },
 ];
+
 
 function Home() {
   return (
     <>
       <HeroComponent />
-   
+      <Cards />
       <ImageGallery galleryImages={galleryImages} />
       <Footer />
     </>
