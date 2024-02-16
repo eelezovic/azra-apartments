09dc60c17162ed8aacbdc06bb39d309c14a3ccfd
eelezovic <elitedesire17@gmail.com>
2024-02-15 21:56:29 -0500

    Updated navbar and logo

---
+++ src/components/RoomInfo.js
@@ -0,0 +1,25 @@
+// RoomInfo.js
+import React from "react";
+
+function RoomInfo() {
+  const roomTypesInfo = {
+    single: {
+      title: "Single Room Apartment",
+      description:
+        "Ideal for 2-3 guests. Includes one queen bed, one single bed, TV, AC, internet, bathroom, small kitchen, and balcony with table and chairs.",
+    },
+    double: {
+      title: "Two Bedroom Apartment",
+      description:
+        "Great for larger families up to 6 people. Includes one room with one queen and one single bed, and a second room with one queen and one single bed. Also includes a bathroom, small kitchen, TV, AC, internet, and private balcony.",
+    },
+  };
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default RoomInfo;
